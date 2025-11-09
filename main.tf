faa184be33e87bea9fef3b5e0360dbdf0b4b7b61
fernando.cavarzare <fernando.cavarzare@gmail.com>
2025-11-09 15:45:14 +0000

    fix: corrige bloco de security_rule no NSG

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.100"
+      version = "~> 3.100"
     }
   }
   required_version = ">= 1.6.0"
@@ -13,76 +12,123 @@ provider "azurerm" {
   features {}
 }
 
-
+############################
+# Resource Group
+############################
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group
   location = var.location
 }
 
+############################
+# Virtual Network
+############################
 resource "azurerm_virtual_network" "vnet" {
-  name = "vnet-${var.vm_name}"
-  location = var.location
-  resource_group_name = var.resource_group
-  address_space = ["10.0.0.0/16"]
+  name                = "vnet-${var.vm_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 }
 
+############################
+# Subnet
+############################
 resource "azurerm_subnet" "subnet" {
-  name = "snet-${var.vm_name}"
-  resource_group_name  = var.resource_group
+  name                 = "snet-${var.vm_name}"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+############################
+# Public IP (Basic, Dynamic)
+############################
 resource "azurerm_public_ip" "pip" {
-  name = "pip-${var.vm_name}"
-  resource_group_name = var.resource_group
-  location = var.location
-  allocation_method = "Dynamic"
-  sku = "Basic"
+  name                = "pip-${var.vm_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
 }
 
+############################
+# Network Security Group
+############################
 resource "azurerm_network_security_group" "nsg" {
-  name = "nsg-${var.vm_name}"
-  location = var.location
-  resource_group_name = var.resource_group
+  name                = "nsg-${var.vm_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-  security_rule { name="allow_ssh"  priority=1001 direction="Inbound" access="Allow" protocol="Tcp"
-    source_port_range="*" destination_port_range="22" source_address_prefix="*" destination_address_prefix="*" }
+  # SSH 22
+  security_rule {
+    name                        = "allow_ssh"
+    priority                    = 1001
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "22"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
 
-  security_rule { name="allow_http" priority=1002 direction="Inbound" access="Allow" protocol="Tcp"
-    source_port_range="*" destination_port_range="80" source_address_prefix="*" destination_address_prefix="*" }
+  # HTTP 80
+  security_rule {
+    name                        = "allow_http"
+    priority                    = 1002
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "80"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
 }
 
+############################
+# Network Interface
+############################
 resource "azurerm_network_interface" "nic" {
-  name = "nic-${var.vm_name}"
-  location = var.location
-  resource_group_name = var.resource_group
+  name                = "nic-${var.vm_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name = "ipcfg"
-    subnet_id = azurerm_subnet.subnet.id
+    name                          = "ipcfg"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
+############################
+# Associate NSG to NIC
+############################
 resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
-  network_interface_id = azurerm_network_interface.nic.id
+  network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
+############################
+# Linux VM (Ubuntu 22.04 LTS)
+############################
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
-  resource_group_name = var.resource_group
+  resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
-  size                = "Standard_B1s"         # elegível Free
+  size                = "Standard_B1s"                # elegível Free Tier
   admin_username      = var.admin_username
   network_interface_ids = [azurerm_network_interface.nic.id]
 
-  admin_ssh_key { username = var.admin_username  public_key = var.ssh_public_key }
+  # chave pública SSH (passe via TF_VAR_ssh_public_key)
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
 
   os_disk {
-    caching = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
@@ -92,8 +138,11 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
-}
 
-output "public_ip"    { value = azurerm_public_ip.pip.ip_address }
-output "ansible_hint" { value = "web ansible_host=${azurerm_public_ip.pip.ip_address} ansible_user=${var.admin_username}" }
+  # opcional: tags
+  tags = {
+    project = "meuapp-nginx"
+    env     = "lab-free"
+  }
+}
 
