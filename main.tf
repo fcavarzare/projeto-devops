2f9166f7e93261d3b85ff2c59db3f00cb7e200ce
fernando.cavarzare <fernando.cavarzare@gmail.com>
2025-11-09 17:01:37 +0000

    corrige imagem Ubuntu 22.04 LTS e dependências

--- main.tf
+++ main.tf
@@ -115,7 +115,7 @@ resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-############################
+#############################
 # Linux Virtual Machine (Ubuntu 22.04 LTS – Free Tier)
 ############################
 resource "azurerm_linux_virtual_machine" "vm" {
