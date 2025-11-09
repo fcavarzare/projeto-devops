34bc4f8fcc015d2338a39c27e27a93e56107f0b8
fernando.cavarzare <fernando.cavarzare@gmail.com>
2025-11-09 16:16:05 +0000

    fix: troca VM B1s por A1_v2 compatível com East US

--- main.tf
+++ main.tf
@@ -118,7 +118,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
-  size                = "Standard_B1s"                # elegível Free Tier
+  size                = "Standard_A1_v2"                # elegível Free Tier
   admin_username      = var.admin_username
   network_interface_ids = [azurerm_network_interface.nic.id]
 
