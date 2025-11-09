68bce5d560d3a9b3864c6c0497b361f4cdb25fd6
fernando.cavarzare <fernando.cavarzare@gmail.com>
2025-11-09 17:58:46 +0000

    adiciona playbook Ansible completo com instalação do Docker e deploy NGINX

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,16 @@
+############################################
+# OUTPUTS.TF — exporta dados da infraestrutura
+############################################
+
+# IP público da VM (para o Ansible e acesso manual)
+output "public_ip" {
+  description = "Endereço IP público da VM criada no Azure"
+  value       = azurerm_public_ip.pip.ip_address
+}
+
+# URL de acesso HTTP à aplicação (para abrir no navegador)
 output "acesso_http" {
-  value = "http://${azurerm_public_ip.pip.ip_address}"
+  description = "URL para acessar a aplicação web"
+  value       = "http://${azurerm_public_ip.pip.ip_address}"
 }
 
